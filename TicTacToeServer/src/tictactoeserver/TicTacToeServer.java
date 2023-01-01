8873a8a4091ba36b6b664f4955aba8cf483b8da7
Nada Ayman <nadaayman9997@gmail.com>
2023-01-01 03:29:42 +0200

    comment at call network

--- TicTacToeServer/src/tictactoeserver/TicTacToeServer.java
+++ TicTacToeServer/src/tictactoeserver/TicTacToeServer.java
@@ -33,17 +33,18 @@ public class TicTacToeServer extends Application {
         Parent root = new SplashBase(stage);
   
         Scene scene = new Scene(root);
-        
+      
         stage.setScene(scene);
         stage.show();
+        
     }
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        new Network();
         launch(args);
+        //new Network();
         
     }
     
