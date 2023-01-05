78bf22f6eec433545982908b6df2f5d9bcc7f039
alaagawish <53160167+alaagawish@users.noreply.github.com>
2023-01-05 10:47:34 +0200

    client-server connection using string

--- TicTacToeServer/src/tictactoeserver/network/ConnectionHandler.java
+++ TicTacToeServer/src/tictactoeserver/network/ConnectionHandler.java
@@ -1,12 +1,13 @@
 package tictactoeserver.network;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import java.util.logging.Level;
@@ -15,32 +16,25 @@ import tictactoeserver.repository.PlayerRepository;
 
 class ConnectionHandler implements Runnable {
 
-    ObjectInputStream objectInputStream;
-    InputStream inputStream;
     Thread thread;
-    OutputStream outputStream;
-    ObjectOutputStream objectOutputStream;
-
+    PrintStream printStream;
+    DataInputStream dataInputStream;
     static Vector<ConnectionHandler> clientsVector = new Vector<ConnectionHandler>();
     static Vector<ConnectionHandler> unavailableClients = new Vector<ConnectionHandler>();
-
-    List<String> messages;
+    String messageSentToClient;
     PlayerRepository playerRepository;
     boolean flag = true, noInput = true;
-    List<String> recievedMessages;
+    String message;
 
     public ConnectionHandler(Socket socket) {
         System.out.println(socket);
-        messages = new ArrayList<>();
         playerRepository = new PlayerRepository();
 
         try {
-            inputStream = socket.getInputStream();
-            objectInputStream = new ObjectInputStream(inputStream);
-            outputStream = socket.getOutputStream();
-            objectOutputStream = new ObjectOutputStream(outputStream);
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            printStream = new PrintStream(socket.getOutputStream());
             ConnectionHandler.clientsVector.add(this);
-            System.out.println("adddd:" + messages + "+" + clientsVector.size());
+            System.out.println("connection build ");
             thread = new Thread(this);
             thread.start();
         } catch (IOException ex) {
@@ -54,77 +48,87 @@ class ConnectionHandler implements Runnable {
         while (true && noInput) {
 
             try {
-               System.out.println(messages);
-                    while (true) {
-                        recievedMessages = (List<String>) objectInputStream.readObject();
-                        if (recievedMessages.size() > 0) {
-                            break;
-                        }
-                    }
-                System.out.println("receive");
-                System.out.println(recievedMessages);
-                if (recievedMessages.size() > 0 && recievedMessages.get(0).equals("Login")) {
-                    String username = recievedMessages.get(1);
-                    String password = recievedMessages.get(2);
-                   
-                    if (playerRepository.login(username, password)) {
-                        messages.add("Login");
-                        messages.add("Login Successfully");
-                        System.out.println("successed");
-//                        objectOutputStream.writeObject(messages);
-                    } else {
-                        messages.add("Login");
-                        messages.add("Login failed");
-                        System.out.println("failed");
-//                        objectOutputStream.writeObject(messages);
-                    }
-                    objectOutputStream.writeObject(messages);
-                    messages.clear();
-                    
-                    recievedMessages.clear();
-//                    messages.add("finish");
-//                    messages.add("finish");
-//                    objectOutputStream.writeObject(messages);
-//                    objectOutputStream.writeObject(messages);
+                String messageReceivedFromClient = dataInputStream.readLine();
+                if (messageReceivedFromClient != null) {
+                    System.out.println(messageReceivedFromClient);
+                    if (messageReceivedFromClient.length() > 0) {
+                        if (messageReceivedFromClient.equals("close")) {
+//                            for (ConnectionHandler client : clientsVector) {
+//                                if (!client.flag) {
+//                                    unavailableClients.add(client);
+//
+//                                }
+                            flag = false;
+                            System.out.println("client closed ddddddddddddddddddddddddd");
+//                            }
 
-                } else if (recievedMessages.size() > 1 && recievedMessages.get(0).equals("close")) {
-                    flag = false;
-                }else{
-                    System.out.println("no  ");
+                            for (ConnectionHandler client : unavailableClients) {
+                                clientsVector.remove(client);
+                                if (clientsVector.isEmpty()) {
+                                    noInput = false;
+                                }
+                            }
+
+                        } else {
+                            String[] arr = messageReceivedFromClient.split(",");
+                            System.out.println(arr);
+                            String username = arr[1];
+                            String password = arr[2];
+                            System.out.println("username: " + username + " password:" + password);
+
+                            if (playerRepository.login(username, password)) {
+                                messageSentToClient = "Login,Successfully";
+                                System.out.println("successed");
+
+                            } else {
+
+                                messageSentToClient = "Login,failed";
+                                System.out.println("failed");
+                            }
+                            printStream.println(messageSentToClient);
+                        }
+                    } else {
+                        try {
+
+                            printStream.close();
+                            dataInputStream.close();
+
+                        } catch (IOException ex1) {
+                            ex1.printStackTrace();
+                        }
+
+                    }
+                    for (ConnectionHandler client : clientsVector) {
+                        if (!client.flag) {
+                            unavailableClients.add(client);
+                        }
+
+                    }
+                    for (ConnectionHandler client : unavailableClients) {
+                        clientsVector.remove(client);
+                        if (clientsVector.size() == 0) {
+                            noInput = false;
+                        }
+
+                    }
+                    messageSentToClient = null;
+
                 }
             } catch (IOException ex) {
                 Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
-//            for (ConnectionHandler client : clientsVector) {
-//                if (!client.flag) {
-//                    unavailableClients.add(client);
-//                }
-//
-//            }
-//            for (ConnectionHandler client : unavailableClients) {
-//                clientsVector.remove(client);
-//                if (clientsVector.size() == 0) {
-//                    noInput = false;
-//                }
-//
-//            }
-        }
 
+        }
     }
 
-    public void closeConnection() throws IOException {
+    public void closeConnection() {
 
         for (ConnectionHandler client : clientsVector) {
 
-            objectOutputStream.writeObject(new Object());
             try {
                 thread.stop();
-                inputStream.close();
-                outputStream.close();
-                objectInputStream.close();
-                objectOutputStream.close();
+                printStream.close();
+                dataInputStream.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
 
